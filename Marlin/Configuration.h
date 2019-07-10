788646b21e450e6fa02afc37423a59b0156c885f
Artjoms Stukans <artjoms.stukans@gmail.com>
2019-07-10 23:52:45 +0200

    update

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -113,7 +113,7 @@
  *
  * :[-1, 0, 1, 2, 3, 4, 5, 6, 7]
  */
-//#define SERIAL_PORT_2 -1
+#define SERIAL_PORT_2 0
 
 /**
  * This setting determines the communication speed of the printer.
@@ -1705,7 +1705,7 @@
 // If you have a speaker that can produce tones, enable it here.
 // By default Marlin assumes you have a buzzer with a fixed frequency.
 //
-//#define SPEAKER
+#define SPEAKER
 
 //
 // The duration and frequency for the UI feedback sound.
